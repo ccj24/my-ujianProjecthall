ed7b96ee6d3bbbc993e4d786027df24d0e620695
ccj24 <784858070@qq.com>
2019-07-24 16:45:46 +0800

    拼音列表

--- src/components/spellChild.vue
+++ src/components/spellChild.vue
@@ -1,42 +1,50 @@
 <template>
   <div>
     <div class="userMessage" v-for="(item,index) in memberlist" :key="index">
-      <img :src="item.Portrait">
+      <img :src="item.Portrait" />
       <ul>
         <li style="color: #353535;">{{item.UserName}}</li>
         <li style="color: #8c8c8c;">岗位：{{item.PostName}}</li>
       </ul>
       <div class="coefficient">
-        <p style="padding-right: 0.7rem;" :class="{'serial1':50<item.Ratio<60,'serial2':60<item.Ratio<80,'serial3':80<item.Ratio<100,}">工作系数{{item.Ratio*100+"%"}}</p>
+        <p
+          style="padding-right: 0.7rem;"
+          :class="{'serial1':50<item.Ratio<60,'serial2':60<item.Ratio<80,'serial3':80<item.Ratio<100,}"
+        >工作系数{{item.Ratio*100+"%"}}</p>
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
 export default {
   props: {
     ProjectMemberList: [],
-    ModelResponse: {}
+    spell: ""
   }, //接收父组件传过来的值
   data() {
     return {};
   },
   computed: {
     memberlist() {
+      // 定义一个新的数组  存放筛选后的数组
       let _memberlist = [];
+      // 循环项目成员的数组
       for (let index = 0; index < this.ProjectMemberList.length; index++) {
         const element = this.ProjectMemberList[index];
-        if (element.DepartmentId == this.ModelResponse.KeywordId) {
+        // 判断传过来的字母是否和项目成员里面的Spelling首字母相同
+        if (element.Spelling[0] == this.spell) {
+          //把符合的项插入新的数组
           _memberlist.push(element);
         }
       }
+      // 最后返回给新的数组
       return _memberlist;
     }
   },
-  async mounted() {
-   
-  }
+
+  mounted() {}
 };
 </script>
 
@@ -47,7 +55,8 @@ export default {
   line-height: 1.5rem;
 }
 .userMessage {
-  border-top: solid 0.1rem #fafafa;
+  background-color: #ffffff;
+  border-top: solid 0.01rem #e8e8e8;
   height: 1.5rem;
   font-size: 0.36rem;
 }
@@ -69,14 +78,13 @@ export default {
   padding-right: 0.2rem;
   overflow: hidden;
 }
-.serial1{
-    color: #ffa800;
+.serial1 {
+  color: #ffa800;
 }
-.serial2{
-    color: #59cf8b;
+.serial2 {
+  color: #59cf8b;
 }
-.serial3{
-    color: #12b7f5;
+.serial3 {
+  color: #12b7f5;
 }
-
 </style>
